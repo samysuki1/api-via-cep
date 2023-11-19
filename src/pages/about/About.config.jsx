b6490e2e6db2c1314f55cc762b66457724766f2b
samysuki1 <samantha-maria1@hotmail.com>
2023-11-19 17:57:37 -0300

    Atualizando sobre mim

--- src/pages/about/About.config.jsx
+++ src/pages/about/About.config.jsx
@@ -1,6 +1,6 @@
 export const ABOUT_CONFIG = {
   TITLE: "Sobre mim",
-  DESCRIPTION: "Olá! Meu nome é Samantha Silva (Samy Suki) tenho 28 anos, possuo ampla experiência em comércio eletrônico (E-commerce), perfil analítico e empreendedor com habilidade de liderar estratégias digitais e transitar por diversas áreas integrando equipes e projetos, sempre atualizada com as principais tendências da transformação digital em busca de contribuir para melhorar a performance dos colaboradores e empresa, proporcionando boas experiências aos clientes/usuários. Estou situada em São Paulo. Clique abaixo para saber informações sobre minha localização:",
+  DESCRIPTION: "Olá! Meu nome é Samantha Silva (Samy Suki) 28 anos, possuo ampla experiência em comércio eletrônico (E-commerce), perfil analítico e empreendedor com habilidade de liderar estratégias digitais e transitar por diversas áreas integrando equipes e projetos, sempre atualizada com as principais tendências da transformação digital em busca de contribuir para melhorar a performance dos colaboradores e empresa, proporcionando boas experiências aos clientes/usuários. Estou situada em São Paulo. Clique abaixo para saber informações sobre minha localização:",
   CEP: "05822-010",
   BUTTON_TEXT: "Consultar"
 };
