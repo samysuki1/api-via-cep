c46b6c378249a0564639991db16a72a41b3b5073
samysuki1 <samantha-maria1@hotmail.com>
2023-11-19 14:52:21 -0300

    Corrigindo o CEP

--- src/pages/about/About.config.jsx
+++ src/pages/about/About.config.jsx
@@ -1,6 +1,6 @@
 export const ABOUT_CONFIG = {
   TITLE: "Sobre mim",
   DESCRIPTION: "Olá! Sou Samantha Silva, coordenadora de e-commerce com experiência em React, HTML5, CSS3 e JS. Estou situada em São Paulo. Clique abaixo para saber informações sobre minha localização:",
-  CEP: "05653-070",
+  CEP: "05822-010",
   BUTTON_TEXT: "Consultar"
 };
